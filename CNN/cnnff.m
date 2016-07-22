994af446fcd2fb15a2d60059d6c4d3a4b11c02db
unknown <vaulttech@gmail.com>
2016-07-22 15:56:45 +0200

    CNN's `kernelsize` doesn't need to be squared anymore
     Changes to be committed:
    	modified:   CNN/cnnff.m
    	modified:   CNN/cnnsetup.m

--- CNN/cnnff.m
+++ CNN/cnnff.m
@@ -8,7 +8,8 @@ function net = cnnff(net, x)
             %  !!below can probably be handled by insane matrix operations
             for j = 1 : net.layers{l}.outputmaps   %  for each output map
                 %  create temp output map
-                z = zeros(size(net.layers{l - 1}.a{1}) - [net.layers{l}.kernelsize - 1 net.layers{l}.kernelsize - 1 0]);
+                z = zeros(size(net.layers{l - 1}.a{1}) - [net.layers{l}.kernelsize(1) - 1 net.layers{l}.kernelsize(2) - 1 0]);
+                %z = zeros(size(net.layers{l - 1}.a{1}) - [net.layers{l}.kernelsize(1) - 1 net.layers{l}.kernelsize(2) - 1]);
                 for i = 1 : inputmaps   %  for each input map
                     %  convolve with corresponding kernel and add to temp output map
                     z = z + convn(net.layers{l - 1}.a{i}, net.layers{l}.k{i}{j}, 'valid');
@@ -22,6 +23,7 @@ function net = cnnff(net, x)
             %  downsample
             for j = 1 : inputmaps
                 z = convn(net.layers{l - 1}.a{j}, ones(net.layers{l}.scale) / (net.layers{l}.scale ^ 2), 'valid');   %  !! replace with variable
+                %z = conv(net.layers{l - 1}.a{j}, ones(net.layers{l}.scale)' ./ (net.layers{l}.scale .^ 2), 'valid');   %  !! replace with variable
                 net.layers{l}.a{j} = z(1 : net.layers{l}.scale : end, 1 : net.layers{l}.scale : end, :);
             end
         end
@@ -32,6 +34,7 @@ function net = cnnff(net, x)
     for j = 1 : numel(net.layers{n}.a)
         sa = size(net.layers{n}.a{j});
         net.fv = [net.fv; reshape(net.layers{n}.a{j}, sa(1) * sa(2), sa(3))];
+        %net.fv = [net.fv; reshape(net.layers{n}.a{j}, sa(1), sa(2))];
     end
     %  feedforward into output perceptrons
     net.o = sigm(net.ffW * net.fv + repmat(net.ffb, 1, size(net.fv, 2)));
--- CNN/cnnsetup.m
+++ CNN/cnnsetup.m
@@ -6,6 +6,7 @@ function net = cnnsetup(net, x, y)
     for l = 1 : numel(net.layers)   %  layer
         if strcmp(net.layers{l}.type, 's')
             mapsize = mapsize / net.layers{l}.scale;
+            %mapsize = mapsize ./ net.layers{l}.scale;
             assert(all(floor(mapsize)==mapsize), ['Layer ' num2str(l) ' size must be integer. Actual: ' num2str(mapsize)]);
             for j = 1 : inputmaps
                 net.layers{l}.b{j} = 0;
@@ -13,9 +14,9 @@ function net = cnnsetup(net, x, y)
         end
         if strcmp(net.layers{l}.type, 'c')
             mapsize = mapsize - net.layers{l}.kernelsize + 1;
-            fan_out = net.layers{l}.outputmaps * net.layers{l}.kernelsize ^ 2;
+            fan_out = net.layers{l}.outputmaps * prod(net.layers{l}.kernelsize);
             for j = 1 : net.layers{l}.outputmaps  %  output map
-                fan_in = inputmaps * net.layers{l}.kernelsize ^ 2;
+                fan_in = inputmaps * prod(net.layers{l}.kernelsize);
                 for i = 1 : inputmaps  %  input map
                     net.layers{l}.k{i}{j} = (rand(net.layers{l}.kernelsize) - 0.5) * 2 * sqrt(6 / (fan_in + fan_out));
                 end
